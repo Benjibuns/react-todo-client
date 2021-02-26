2b6e4b590c074f85085f4799dbfecebe1bd48dec
Benjamin Rivera <benriveracoding@gmail.com>
2021-02-26 15:58:37 -0700

    add state for title, form with an input and button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = {
+      title: "",
+    };
   }
-  render() { 
-    return ( <div className="app">
-      <h1>Hello</h1>
-    </div> );
+
+  handleChange = (e) => {
+    this.setState({
+      title: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <h1>Todo List</h1>
+        <form onSubmit={this.handleSubmit} className="add-todo">
+          <input
+            type="text"
+            placeholder="Add Todo"
+            onChange={this.handleChange}
+            value={this.state.title}
+          />
+          <button type="submit">Add</button>
+        </form>
+      </div>
+    );
   }
 }
- 
+
 export default App;
