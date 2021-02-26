8f4521525daf4a50a8a0aa8cb924a048f177e31d
Benjamin Rivera <benriveracoding@gmail.com>
2021-02-26 15:48:03 -0700

    update planning and app.js to class component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
-}
+import React, { Component } from 'react';
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return ( <div className="app">
+      <h1>Hello</h1>
+    </div> );
+  }
+}
+ 
 export default App;
